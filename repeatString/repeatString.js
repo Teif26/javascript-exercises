27f5225f01f1e740ba3a3fd2887e2a076d983999
Andrew Dinsmore <teif26@outlook.com>
2019-01-13 20:03:26 -0500

    Repeated strings finished, all jasmine passed

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = (str,num) => {
 
-}
+if(num < 0){
+    return "ERROR";
+};
 
+if(num === 0){
+return "" ;
+};
+if(num === 1){
+    return str;
+}else {
+    return str + repeatString(str, num - 1);
+};
+
+ 
+};
 module.exports = repeatString
