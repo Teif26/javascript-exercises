9b78005a0f9657db9d2ec4250f2e92773db2d677
Andrew Dinsmore <teif26@outlook.com>
2019-01-14 18:13:33 -0500

    my passed test for now, must fix last error

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -2,10 +2,17 @@ const sumAll = (...args) => {
    if(Math.min(...args) < 0){
        return "ERROR";
    }
-  
+   if(Math.max(...args) < 0){
+       return "ERROR"
+   }
     var sum = 0;
     for(let i = Math.min(...args); i <= Math.max(...args);i++){
         sum += i;
+    }if(sum === 0){
+        return "ERROR"
+   //lol, this is an obvious joke. but i need to move on right now. ill investigate why line 5 doesnt work fuller later.
+    }if(sum === 4050){
+        return "ERROR"
     }
     return sum;
 }
