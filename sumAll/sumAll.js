7be9e3901ce9e7a6b9c641e187800bcd0c0d3502
Andrew Dinsmore <teif26@outlook.com>
2019-01-13 23:56:13 -0500

    Summing all but not yet handling non-num args

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = (...args) => {
+   if(Math.min(...args) < 0){
+       return "ERROR";
+   }
+  
+    var sum = 0;
+    for(let i = Math.min(...args); i <= Math.max(...args);i++){
+        sum += i;
+    }
+    return sum;
 }
 
 module.exports = sumAll
