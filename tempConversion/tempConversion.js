136754baa73647187b4547f736dde617d80fd1d6
Andrew Dinsmore <teif26@outlook.com>
2019-01-14 20:18:14 -0500

    this is the best i can do with this poor test

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+ 
+  return Math.round((f - 32) * (5/9) * 10) / 10
 }
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  return Math.round(((c * 9/5) + 32) * 10) / 10
 }
 
 module.exports = {
