47166af4bef98ba463ca0c256294bfcc2fa09170
Andrew Dinsmore <teif26@outlook.com>
2019-01-27 08:36:15 -0500

    finished, cool solution

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+let newArr = str.toLowerCase().split("").reverse().join("").replace(/[^a-z]/g,"")
+let newStr = str.toLowerCase().replace(/[^a-z]/g,"")
+for(let i=0;i<newArr.length;i++){
+    if(newArr[i]===newStr[i]){
+        return true
+    }else{
+        return false
+    }
 }
 
+}
+//console.log(palindromes('A car, a man, a maraca.'))
 module.exports = palindromes
